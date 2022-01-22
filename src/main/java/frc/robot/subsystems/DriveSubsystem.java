7c40d12abb6e4f18cd0802036c2a89d5221a2732
Robovikes <robovikes@geneva304.org>
2022-01-22 15:56:49 -0600

    Added Drive Subsystem

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class DriveSubsystem extends SubsystemBase {
+    private PWMTalonFX motorLeftFront;
+    private PWMTalonFX motorRightFront;
+    private PWMTalonFX motorLeftBack;
+    private PWMTalonFX motorRightBack;
+    private ADIS16448_IMU gyro;
+
+    public MotorControllerGroup driveLeft;
+    public MotorControllerGroup driveRight;
+    public DifferentialDrive differentialDrive;
+
+  /** Creates a new ExampleSubsystem. */
+  public DriveSubsystem(
+    PWMTalonFX motorLeftFront, PWMTalonFX motorRightFront,
+    PWMTalonFX motorLeftBack, PWMTalonFX motorRightBack,
+    ADIS16448_IMU gyro) {
+      this.motorLeftFront = motorLeftFront;
+      this.motorRightFront = motorRightFront;
+      this.motorLeftBack = motorLeftBack;
+      this.motorRightBack = motorRightBack;
+      this.gyro = gyro;
+
+      driveLeft = new MotorControllerGroup(motorLeftFront, motorLeftBack);
+      driveRight = new MotorControllerGroup(motorRightFront, motorRightBack);
+      differentialDrive = new DifferentialDrive(driveLeft, driveRight);
+    }
+}
