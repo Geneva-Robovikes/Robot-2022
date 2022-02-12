6b74aa3b029234abf8603cb7eb6c73ea40ee70cf
Robovikes <robovikes@geneva304.org>
2022-02-12 16:55:11 -0600

    Added Trajectory Following code

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,6 +7,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
@@ -15,6 +20,7 @@ public class DriveSubsystem extends SubsystemBase {
     private final WPI_TalonFX motorRightFront = new WPI_TalonFX(2);
     private final WPI_TalonFX motorLeftBack = new WPI_TalonFX(1);
     private final WPI_TalonFX motorRightBack = new WPI_TalonFX(3);
+    private final ADIS16448_IMU gyro = new ADIS16448_IMU();
 
 
     private MotorControllerGroup driveLeft = new MotorControllerGroup(motorLeftFront, motorLeftBack);
@@ -22,6 +28,8 @@ public class DriveSubsystem extends SubsystemBase {
     private DifferentialDrive differentialDrive;
 
     private XboxController controller;
+
+    private final DifferentialDriveOdometry odometry;
 
     public DriveSubsystem(){
       driveRight.setInverted(true);
@@ -31,6 +39,7 @@ public class DriveSubsystem extends SubsystemBase {
       motorRightBack.setSafetyEnabled(false);
       differentialDrive = new DifferentialDrive(driveLeft, driveRight);
       differentialDrive.setSafetyEnabled(false);
+      odometry = new DifferentialDriveOdometry(new Rotation2d(gyro.getGyroRateX(), gyro.getGyroAngleY()));
     }
 
     public void tankDrive (double leftSpeed, double rightSpeed){
@@ -47,5 +56,41 @@ public class DriveSubsystem extends SubsystemBase {
     
     public double getLeftY() {
       return controller.getLeftY();
+    }
+
+    public double getEncoderMeters(WPI_TalonFX motor) {
+      double wheelDiameter = 0.1524;
+      double gearRatio = 10.71;
+      double unitsPerRevolution = 2048;
+      return motor.getSelectedSensorPosition() / (gearRatio * unitsPerRevolution / (Math.PI * wheelDiameter));
+    }
+
+    @Override
+    public void periodic() {
+        odometry.update(new Rotation2d(gyro.getGyroRateX(), gyro.getGyroAngleY()), getEncoderMeters(motorLeftFront), getEncoderMeters(motorRightFront));
+    }
+
+    public Pose2d getPose() {
+      return odometry.getPoseMeters();
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds(){
+      return new DifferentialDriveWheelSpeeds(motorLeftFront.getSelectedSensorVelocity(), motorRightFront.getSelectedSensorVelocity());
+    }
+
+    public void ResetOdometry(Pose2d pose){
+      motorRightFront.setSelectedSensorPosition(0);
+      motorLeftFront.setSelectedSensorPosition(0);
+      odometry.resetPosition(pose, new Rotation2d(gyro.getGyroRateX(), gyro.getGyroAngleY()));
+    }
+
+    public double getHeading() {
+      return new Rotation2d(gyro.getGyroRateX(), gyro.getGyroAngleY()).getDegrees();
+    }
+
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+      driveLeft.setVoltage(leftVolts);
+      driveRight.setVoltage(rightVolts);
+      differentialDrive.feed();
     }
 }
