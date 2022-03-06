9c61913659df1d7ddffa1071be2d9dd626c2cbb9
Robovikes <robovikes@geneva304.org>
2022-03-06 15:27:17 -0600

    moved things around

---
+++ src/main/java/frc/robot/commands/Auto/AutoIntakeCommand.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Auto;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+
+/** An example command that uses an example subsystem. */
+public class AutoIntakeCommand extends CommandBase {
+  private final IntakeSubsystem intakeSubsystem;
+  private Timer timer = new Timer();
+  private double waitTime = 1.5;
+  //private final DriveSubsystem driveSubsystem;
+   /*
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public AutoIntakeCommand(IntakeSubsystem subsystem) {
+    intakeSubsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intakeSubsystem.setRollerMotor(.4);
+    intakeSubsystem.setBeltMotor(0.75);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(timer.get() > waitTime) {
+      intakeSubsystem.setRollerMotor(0);
+      intakeSubsystem.setBeltMotor(0);
+    }
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intakeSubsystem.setRollerMotor(0);
+    intakeSubsystem.setBeltMotor(0);
+  }
+}
---
+++ src/main/java/frc/robot/commands/Teleop/BeltCommand.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Teleop;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+
+/** An example command that uses an example subsystem. */
+public class BeltCommand extends CommandBase {
+  private final IntakeSubsystem intakeSubsystem;
+  //private final DriveSubsystem driveSubsystem;
+   /*
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public BeltCommand(IntakeSubsystem subsystem) {
+    intakeSubsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intakeSubsystem.setBeltMotor(.8);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intakeSubsystem.setBeltMotor(0);
+  }
+}
