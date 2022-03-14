cb7951f953f630e2685117e1f1735592decec36e
Robovikes <robovikes@geneva304.org>
2022-03-14 18:10:22 -0500

    Potential replay code

--- src/main/java/frc/robot/commands/Auto/ReplayTeleop.java
+++ src/main/java/frc/robot/commands/Auto/ReplayTeleop.java
@@ -6,8 +6,16 @@ package frc.robot.commands.Auto;
 
 import frc.robot.subsystems.DriveSubsystem;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -16,6 +24,20 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class ReplayTeleop extends CommandBase {
   private final DriveSubsystem driveSubsystem;
   private Timer timer = new Timer();
+  private int currentPoint = 0;
+  private Points[] pointArray;
+
+  class Points {
+    private final double time;
+    private final double leftVolts;
+    private final double rightVolts;
+
+    public Points(double time, double leftVolts, double rightVolts) {
+      this.time = time;
+      this.leftVolts = leftVolts;
+      this.rightVolts = rightVolts;
+    }
+  }
   /**
    * Creates a new ExampleCommand.
    *
@@ -30,22 +52,37 @@ public class ReplayTeleop extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    timer.start();
     String filename = "";
-    Path filePath = Filesystem.getDeployDirectory().toPath().resolve("Recordings/" + filename + ".json");
+    try {
+      Path filePath = Filesystem.getDeployDirectory().toPath().resolve("Recordings/" + filename + ".json");
+      Reader reader = Files.newBufferedReader(filePath, StandardCharsets.UTF_8);
+      Gson gson = new GsonBuilder().create();
+      pointArray = gson.fromJson(reader, Points[].class);
+    } catch (IOException exception) {
+      DriverStation.reportError("Oopsie!", exception.getStackTrace());
+    }
+    timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if(pointArray[currentPoint].time > timer.get()) {
+      driveSubsystem.tankDriveVolts(pointArray[currentPoint].leftVolts, pointArray[currentPoint].rightVolts);
+      currentPoint++;
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    driveSubsystem.tankDrive(0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if(pointArray.length-1 == currentPoint) { return true; }
     return false;
   }
 }
