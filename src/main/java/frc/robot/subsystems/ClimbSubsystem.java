285f3b4ea52195ef3adbddaf54f1e8ff95ccad0f
Robovikes <robovikes@geneva304.org>
2022-03-16 15:24:35 -0500

    added talons for climb

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -8,9 +9,16 @@ public class ClimbSubsystem extends SubsystemBase {
     private WPI_VictorSPX climbLeft;
     private WPI_VictorSPX climbRight;
 
+    //private TalonFX climbLeft;
+    //private TalonFX climbRight;
+    //make sure new talons are updated
+
     public ClimbSubsystem() {
         climbLeft = new WPI_VictorSPX(8);
         climbRight = new WPI_VictorSPX(9);
+
+        //climbLeft = new TalonFX(8);
+        //climbRight = new TalonFX(9);
     }
 
     public void setClimbMotors(double speed) {
