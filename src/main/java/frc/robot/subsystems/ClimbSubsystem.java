bf25ca5f464666fdbd3869e981cddf3e0cf5c699
Robovikes <robovikes@geneva304.org>
2022-03-17 10:12:50 -0500

    Climb Switched to Falcon 500

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,24 +1,16 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
-    private WPI_VictorSPX climbLeft;
-    private WPI_VictorSPX climbRight;
-
-    //private TalonFX climbLeft;
-    //private TalonFX climbRight;
-    //make sure new talons are updated
-
+    private WPI_TalonFX climbLeft;
+    private WPI_TalonFX climbRight;
+    
     public ClimbSubsystem() {
-        climbLeft = new WPI_VictorSPX(8);
-        climbRight = new WPI_VictorSPX(9);
-
-        //climbLeft = new TalonFX(8);
-        //climbRight = new TalonFX(9);
+        climbLeft = new WPI_TalonFX(8);
+        climbRight = new WPI_TalonFX(9);
     }
 
     public void setClimbMotors(double speed) {
